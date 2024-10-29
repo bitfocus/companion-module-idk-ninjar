3e4b869caae689fa8bda440e1461b467efab97b5
Joseph Adams <josephdadams@gmail.com>
2024-10-28 22:05:20 -0400

    add `ssw` command (#2)

--- src/actions.js
+++ src/actions.js
@@ -126,6 +126,36 @@ module.exports = {
 			},
 		}
 
+		actions.switchVideoAndAudioChannel = {
+			name: 'Switch Video and Audio Channel',
+			description: 'Switch the output video and audio channel to a specific input channel',
+			options: [
+				{
+					type: 'number',
+					label: 'Input Channel Number',
+					id: 'input',
+					default: 1,
+					min: 1,
+					max: 512,
+					required: true,
+				},
+				{
+					type: 'number',
+					label: 'Output Channel Number',
+					id: 'output',
+					default: 1,
+					min: 1,
+					max: 512,
+					required: true,
+				},
+			],
+			callback: async function (action) {
+				let options = action.options
+				let command = `@SSW,0,0,1,${options.input},${options.output}`
+				self.sendCommand(command)
+			},
+		}
+
 		actions.sendCustomCommand = {
 			name: 'Send Custom Command',
 			description: 'Send a custom command to the device',
